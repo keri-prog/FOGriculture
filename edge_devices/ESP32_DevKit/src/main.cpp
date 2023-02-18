a72480b3c2f0e4d36adb4c2f4879bb72a5e700b5
Kush Ojha <kushojha@pop-os.localdomain>
2023-02-18 19:01:26 +0530

    feat: DHT running for ESP32

--- edge_devices/ESP32_DevKit/src/main.cpp
+++ edge_devices/ESP32_DevKit/src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include <math.h>
+#include <string.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHT_PIN 13
+#define DHT_TYPE DHT11
+
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(5000);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 }
