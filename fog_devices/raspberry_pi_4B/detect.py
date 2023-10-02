55460465737e84bf8fa3d2b02ce19afdddbf07dc
Kush Ojha <kushojha@pop-os.localdomain>
2023-10-02 21:15:15 +0530

    feat: raspi and esp32 server interfacing for buzzer

--- fog_devices/raspberry_pi_4B/detect.py
+++ fog_devices/raspberry_pi_4B/detect.py
@@ -1,9 +1,9 @@
+import os
+from dotenv import load_dotenv, find_dotenv
 import cv2
 import torch
-import serial 
+import requests
 
-BAUD_RATE = 115200
-SERIAL_PORT = '/dev/ttyUSB0'
 CLASSES = {
     0: "Bear",
     1: "Cattle",
@@ -19,50 +19,72 @@ CLASSES = {
     11: "Snake",
     12: "Squirrel",
     13: "Rabbits",
-    14: "Pig"
+    14: "Pig",
 }
+_ = load_dotenv(find_dotenv())
+URL = os.environ["URL"]
+buzzer_on = 0
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path="/home/kushojha/FOGriculture/model/weights/best.pt")
-# vid = cv2.VideoCapture("../../../Downloads/mixkit-two-elephants-on-the-banks-of-a-river-3660-medium.mp4")
+model = torch.hub.load(
+    "ultralytics/yolov5",
+    "custom",
+    path="/home/kushojha/FOGriculture/model/weights/best.pt",
+)
 vid = cv2.VideoCapture(0)
-ser = serial.Serial(port = SERIAL_PORT, baudrate=BAUD_RATE)
+
 
 def bbox2rect(model, img_data):
-        results = model(cv2.cvtColor(img_data, cv2.COLOR_BGR2RGB), size = 640)
-        labels = results.xyxyn[0][:, -1].cpu().numpy()
-        n = len(labels)
-        cord = results.xyxyn[0][:, :-1].cpu().numpy()
-        x_shape, y_shape = img_data.shape[1], img_data.shape[0]
-        # print(cord, labels)
-        rects = []
-        for i in range(n):
-            row = cord[i]
-            conf = row[4]
-            # If score is less than 0.4 we avoid making a prediction.
-            if conf < 0.4: 
-                continue
-            x1 = int(row[0]*x_shape)
-            y1 = int(row[1]*y_shape)
-            x2 = int(row[2]*x_shape)
-            y2 = int(row[3]*y_shape)
-            rects.append((x1,y1,x2,y2, conf))
-        return rects, labels
+    results = model(cv2.cvtColor(img_data, cv2.COLOR_BGR2RGB), size=640)
+    labels = results.xyxyn[0][:, -1].cpu().numpy()
+    n = len(labels)
+    cord = results.xyxyn[0][:, :-1].cpu().numpy()
+    x_shape, y_shape = img_data.shape[1], img_data.shape[0]
+    # print(cord, labels)
+    rects = []
+    for i in range(n):
+        row = cord[i]
+        conf = row[4]
+        # If score is less than 0.4 we avoid making a prediction.
+        if conf < 0.4:
+            continue
+        x1 = int(row[0] * x_shape)
+        y1 = int(row[1] * y_shape)
+        x2 = int(row[2] * x_shape)
+        y2 = int(row[3] * y_shape)
+        rects.append((x1, y1, x2, y2, conf))
+    return rects, labels
 
-while(True):
-    
+
+i = 0
+while True:
     ret, frame = vid.read()
-    
+    i += 1
+    if i % 4 != 0:
+        continue
     rects, detections = bbox2rect(model, frame)
     for (x1, y1, x2, y2, conf), label in zip(rects, detections):
-        conf_label = str(round(conf, 2))+" "+ CLASSES[int(label)]
+        conf_label = str(round(conf, 2)) + " " + CLASSES[int(label)]
         cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-        cv2.putText(frame, conf_label,(x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-        ser.write(bytes(CLASSES[int(label)], "utf-8")) 
-    
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.putText(
+            frame,
+            conf_label,
+            (x1, y1),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 0, 0),
+            2,
+            cv2.LINE_AA,
+        )
+    print(len(detections))
+    if len(detections) >= 1 and not buzzer_on:
+        response = requests.get(URL, params={"state": 1})
+        buzzer_on = 1
+    elif len(detections) == 0 and buzzer_on:
+        response = requests.get(URL, params={"state": 0})
+        buzzer_on = 0
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 vid.release()
 cv2.destroyAllWindows()
-ser.close()
